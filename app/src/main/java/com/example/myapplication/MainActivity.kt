5f3961f7766ed68c391b224da47455cd1854053c
a.naga <shinamonphon+1@gmail.com>
2020-02-20 23:26:12 +0900

    [add] Intentを使ってNext画面に画面遷移。

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
@@ -25,13 +27,19 @@ class MainActivity : AppCompatActivity() {
             helloTextView.text = "押されたよ"
         }
 
-
-
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
     }
+
+    public fun onClickButonnnnn(view: View) {
+        val intent: Intent = Intent(this, NextActivity::class.java)
+        Intent(this, NextActivity::class.java)
+        startActivity(intent)
+
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
